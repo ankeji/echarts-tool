e369992c8f92bd1304e60511e76ba6733a47485d
ankeji <ankeji1995@163.com>
2020-06-29 18:47:45 +0800

    布局编辑

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: ankeji
+ * @Date: 2020-06-29 17:07:10
+ * @LastEditors: ankeji
+ * @LastEditTime: 2020-06-29 18:29:53
+ */ 
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+Vue.use(Antd);
 
 Vue.config.productionTip = false
 
